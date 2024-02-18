bbe68926923e11ed11e41a79cc16503e0ad04715
井原 樹/Miki Ihara <kimity0824@icloud.com>
2024-02-19 04:48:59 +0900

    fixed region

--- infra/terraform/gcp/production/main.tf
+++ infra/terraform/gcp/production/main.tf
@@ -10,6 +10,35 @@ module "project_services" {
   enable_apis = var.enable_apis
 }
 
+# resource "google_cloud_run_service" "cloud_run" {
+#   name     = "example-service"
+#   location = "asia-northeast1" # コンテナイメージと同じリージョンを指定
+#   project  = var.project_id
+
+#   template {
+#     spec {
+#       containers {
+#         image = "asia.gcr.io/cloud-run-terraform-sample/api-production-jpe"
+#       }
+#     }
+#     metadata {
+#     annotations = {
+#             "autoscaling.knative.dev/minScale" = 0,
+#             "autoscaling.knative.dev/maxScale" = 10
+#         }
+#     }
+#   }
+
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+
+#   autogenerate_revision_name = true
+  
+# }
+
+
 resource "google_artifact_registry_repository" "my_repository" {
   provider = google
   location = var.default_region
